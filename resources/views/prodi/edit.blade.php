474a07cbec2844b5a876475e5e6f1c170e832f56
Zuki1273 <barito279@gmail.com>
2025-07-04 22:31:46 +0800

    Prodi

--- resources/views/prodi/edit.blade.php
+++ resources/views/prodi/edit.blade.php
@@ -8,10 +8,10 @@
           <div class="container-fluid">
             <!--begin::Row-->
             <div class="row">
-              <div class="col-sm-6"><h3 class="mb-0">Data Mahasiswa</h3></div>
+              <div class="col-sm-6"><h3 class="mb-0">Data Prodi</h3></div>
               <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-end">
-                  <li class="breadcrumb-item"><a href="/mahasiswa">Data Prodi</a></li>
+                  <li class="breadcrumb-item"><a href="/mahasiswa">Data Mahasiswa</a></li>
                   <li class="breadcrumb-item"><a href="/prodi">Program Studi</a></li>
                   <li class="breadcrumb-item active" aria-current="page">Edit</li>
                 </ol>
@@ -30,7 +30,7 @@
             <div class="row">
               <div class="col-md-12">
                 <div class="card mb-4">
-                  <div class="card-header"><h3 class="card-title">Edit Mahasiswa</h3></div>
+                  <div class="card-header"><h3 class="card-title">Edit Prodi</h3></div>
                   <!-- /.card-header -->
                   <form action="{{ url('prodi/'.$prodi->id) }}" method="post" enctype="multipart/form-data">
                     @method('PUT')
