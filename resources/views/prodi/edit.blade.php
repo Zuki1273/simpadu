61177bdc938297d9ba2d67ba8024e74f8a057ca3
Zuki1273 <barito279@gmail.com>
2025-07-04 22:24:29 +0800

    Prodi

--- resources/views/prodi/edit.blade.php
+++ resources/views/prodi/edit.blade.php
@@ -11,7 +11,7 @@
               <div class="col-sm-6"><h3 class="mb-0">Data Mahasiswa</h3></div>
               <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-end">
-                  <li class="breadcrumb-item"><a href="/mahasiswa">Data Mahasiswa</a></li>
+                  <li class="breadcrumb-item"><a href="/mahasiswa">Data Prodi</a></li>
                   <li class="breadcrumb-item"><a href="/prodi">Program Studi</a></li>
                   <li class="breadcrumb-item active" aria-current="page">Edit</li>
                 </ol>
